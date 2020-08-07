e0ac1a117b0388524aa74a420a1a5fd051337afe
Samson <reytris666@gmail.com>
2020-08-07 15:53:58 +0300

    browsing, routers

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg"></img>
             </div>
