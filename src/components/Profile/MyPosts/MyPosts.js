68bbd29109017a77ef71682388412c06d60c5076
Samson <reytris666@gmail.com>
2020-08-07 18:25:58 +0300

    BLL

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post'
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hi, how are you', likesCount: 12},
+        {id: 2, message: "It's my first message", likesCount: 17},
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you" likecount="15"/>
-                <Post message="It's my first message" likecount="25"/>
+                <Post message={postData[0].message} likecount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likecount={postData[1].likesCount}/>
             </div>
         </div>
 
